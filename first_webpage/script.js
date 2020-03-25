04cee36d2651a64740a58471901e74c8fa226734
kehinde <kehindesalaudeen222@gmail.com>
2020-03-26 00:34:15 +0100

    js task

--- first_webpage/script.js
+++ first_webpage/script.js
@@ -1,3 +1,12 @@
+//sotre name in an array
+  var name = "kenny";
+//store courses in an array
+var courses = ['html', 'css' 'javascript'];
+console.log(name);
+document.write(name);
+console.log(courses);
+document.write(courses);
+//write even nums from 0 - 200 inclusive
 for (var i = 0; i <=200; i++) {
   if (i%2 == 0 ) {
     console.log(i);
